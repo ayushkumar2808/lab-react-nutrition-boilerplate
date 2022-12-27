5f662a4c559918e13e15605c107f01b338f89bbf
Ayush Kumar <115335623+Ayush-28-08@users.noreply.github.com>
2022-12-27 16:19:19 +0530

    calorie count done

---
+++ pro-nutrition/src/components/main.js
@@ -0,0 +1,87 @@
+import React,{Component} from 'react'
+import '../components/main.css'
+import { Fooddata } from './resources/FoodData'
+class Main extends Component{
+
+  constructor(){
+    super()
+    this.handleChange = this.handleChange.bind(this);
+    this.state={
+      text: 0,
+    }
+  }
+  
+  handleChange(e){
+    this.setState({text: e.target.value});
+    
+
+}
+  getCalories(e){
+    
+    this.setState({
+      text:this.state.text
+    })
+    for(let i=0;i<Fooddata.length;i++){
+      if(Fooddata[i].id===e){
+        Fooddata[e].cal=this.state.text*Fooddata[e].finalCal
+
+      }
+    }
+
+
+  }
+  reset(e){
+    console.log(e)
+    this.setState({
+      text:this.state.text
+    })
+    for(let i=0;i<Fooddata.length;i++){
+      if(Fooddata[i].id===e){
+        Fooddata[e].cal=0
+
+      }
+    }
+
+  }
+
+    render(){
+        return(
+
+          <>
+          <div id='top-div'>
+            <h2>Search</h2>
+            <input type="search" />
+            </div>
+            {Fooddata.map((item) => {
+
+            return <div id='full-content'>
+            <div><img id='food-img' src={item.img} alt="" /></div>
+            <div className="content">
+        <p>
+          <strong>{item.name}</strong> <br />
+          <small>{item.finalCal}</small>
+        </p>
+      </div>
+      <div id='input-and-button'>
+        <div>
+        <input type="number" onChange={this.handleChange} />
+        </div>
+        <div>
+          <button onClick={()=>this.getCalories(item.id)}>+</button>
+        </div>
+       
+      </div>
+      <div id='total-calories'>
+        <h3> Total Calories = {item.cal}</h3>
+        <button id='reset-button' onClick={()=>this.reset(item.id)}>Reset</button>
+      </div>
+
+          </div>
+             }) }
+          </>
+        )
+    }
+
+}
+
+export default Main
--- pro-nutrition/src/components/resources/FoodData.js
+++ pro-nutrition/src/components/resources/FoodData.js
@@ -1,32 +1,40 @@
-export default [{
+
+
+export const Fooddata = [
+  {
   id:0,
   img:"https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/pizza-react.jpg",
   name:"pizza",
   cal:266,
+  finalCal:266,
 },
 {
   id:1,
   img:"https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/applie-react.jpeg",
   name:"apple",
   cal:52,
+  finalCal:52,
 },
 {
-  id:3,
+  id:2,
   img:"https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/orange-react.jpeg",
   name:"oranges",
   cal:47,
+  finalCal:47,
 },
 {
-  id:4,
+  id:3,
   img:"https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/almonds-react.jpeg",
   name:"almonds",
   cal:7,
+  finalCal:7
 },
 {
-  id:5,
+  id:4,
   img:"https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/burger-react.jpeg",
   name:"burger",
   cal:295,
-},
+  finalCal:295,
+}
 
 ]
